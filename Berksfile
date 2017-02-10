7b7185eb4e4e3f2357d668ada2d72e3dd8229541
Will Fisher <will@gina.alaska.edu>
2017-02-09 15:15:51 -0900

    updating for new certificates

--- Berksfile
+++ Berksfile
@@ -1,15 +1,4 @@
 source "https://supermarket.chef.io"
+source :chef_server
 
 metadata
-
-cookbook 'chef-vault'
-cookbook 'postgresql'
-cookbook 'database'
-cookbook 'rubies', '~> 0.1.0'
-cookbook 'runit'
-cookbook 'git'
-cookbook 'user'
-cookbook 'nodejs', '~> 2.4.0'
-cookbook 'nginx'
-cookbook 'certificate', '~> 1.0.0'
-cookbook 'backup', '~> 1.3.0'
--- recipes/_db_backup.rb
+++ recipes/_db_backup.rb
@@ -2,6 +2,16 @@
 include_recipe 'chef-vault'
 app = chef_vault_item('apps', node['app']['data_bag'])
 
+node.default['backup']['version'] = '4.3.0'
+package %w( libxml2 libxml2-devel ) do
+  action :upgrade
+end
+
+chef_gem 'nokogiri' do
+  version '1.6.6.2'
+  options '-- --use-system-libraries --with-xml2-config=/usr/bin/xml2-config'
+end
+
 include_recipe 'backup::default'
 
 backup_model :postgresql do
